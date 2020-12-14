e46473a75f480064b70dfcc506d2ab36c2a5266e
brudolce <brudolce@gmail.com>
2020-12-14 00:00:31 -0300

    build up

---
+++ build/precache-manifest.f683c5ba405b72b2c846252fb9d49778.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb8ee7b43cd4f855e52e2295dbcc1d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2200af89a83d5b44ffa",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d2200af89a83d5b44ffa",
+    "url": "/static/js/2.51c10cce.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.51c10cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58457782540e0bb4ba7b",
+    "url": "/static/js/main.6767c857.chunk.js"
+  },
+  {
+    "revision": "b23ef90b421cf9380bfa",
+    "url": "/static/js/runtime-main.7ab8035c.js"
+  },
+  {
+    "revision": "df9b7468f561d3e882a4a86eff2870f9",
+    "url": "/static/media/bg1.df9b7468.jpg"
+  },
+  {
+    "revision": "48df46fcc2f9c2bacd1f257446ecdf2a",
+    "url": "/static/media/kitchen-tools.48df46fc.svg"
+  },
+  {
+    "revision": "c32d5bb64111fd4e2220945536ea4a3f",
+    "url": "/static/media/user.c32d5bb6.svg"
+  }
+]);
